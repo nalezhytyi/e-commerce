91a96df335fb32024a9c9771dcbbc82ca32b673b
nalezhytyi <kawory05@gmail.com>
2020-06-29 23:08:06 +0300

    implemented firebase utils, included ability to store authenteficated users

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/ShopPage';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/header/Header';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/SignInAndSignUp';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
     constructor(props) {
@@ -18,10 +18,23 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user })
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth)
 
-            console.log(user)
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    })
+                    console.log(this.state)
+                })
+            } else {
+                this.setState({ currentUser: userAuth })
+            }
+
         })
     }
 
--- src/pages/sign-in-and-sign-up/SignInAndSignUp.jsx
+++ src/pages/sign-in-and-sign-up/SignInAndSignUp.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './SignInAndSignUp.styles.scss'
 import SignIn from '../../components/sign-in/SignIn';
+import SignUp from '../../components/sign-up/SignUp';
 
 const SignInAndSignUp = props => {
     return (
         <div className='sign-in-and-sign-up'>
             <SignIn />
+            <SignUp />
         </div>
     )
 }
