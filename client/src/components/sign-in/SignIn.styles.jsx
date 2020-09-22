53712b3ae02c0534c3992c59fd2b3d7df6185a65
nalezhytyi <kawory05@gmail.com>
2020-09-22 22:31:08 +0300

    responsive fix

--- client/src/components/sign-in/SignIn.styles.jsx
+++ client/src/components/sign-in/SignIn.styles.jsx
@@ -5,6 +5,10 @@ export const SignInContainer = styled.div`
   width: 380px;
   display: flex;
   flex-direction: column;
+  
+    @media screen and (max-width: 800px) {
+       width: 95%;
+    }
 `
 export const Title = styled.h2`
   margin: 10px 0;
--- client/src/components/sign-up/SignUp.styles.jsx
+++ client/src/components/sign-up/SignUp.styles.jsx
@@ -4,6 +4,10 @@ export const SignUpContainer = styled.div`
   width: 380px;
   display: flex;
   flex-direction: column;
+  
+  @media screen and (max-width: 800px) {
+       width: 95%;
+    }
 `
 export const Title = styled.h2`
   margin: 10px 0;
