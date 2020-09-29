ee9d80ec870a524645c2f228cc6fe576337e7564
nalezhytyi <kawory05@gmail.com>
2020-09-29 23:28:19 +0300

    gitignore fix

--- client/src/redux/cart/cart.utils.js
+++ client/src/redux/cart/cart.utils.js
@@ -1,29 +1,21 @@
 export const addItemToCart = (cartItem, cartItemToAdd) => {
-    const existingCartItem = cartItem.find(
-        cartItem => cartItem.id === cartItemToAdd.id
+  const existingCartItem = cartItem.find((cartItem) => cartItem.id === cartItemToAdd.id);
+  if (existingCartItem) {
+    return cartItem.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
     );
-    if (existingCartItem) {
-        return cartItem.map(
-            cartItem => cartItem.id === cartItemToAdd.id ?
-                { ...cartItem, quantity: cartItem.quantity + 1 }
-                : cartItem
-        )
-    }
-    return [...cartItem, { ...cartItemToAdd, quantity: 1 }]
+  }
+  return [...cartItem, { ...cartItemToAdd, quantity: 1 }];
 };
 
 export const removeItemFromCart = (cartItem, cartItemToRemove) => {
-    const existingCartItem = cartItem.find(
-        cartItem => cartItem.id === cartItemToRemove.id
-    );
-    if (existingCartItem.quantity === 1) {
-        return cartItem.filter(
-            cartItem => cartItem.id !== cartItemToRemove.id
-        )
-    }
-    return cartItem.map(
-        cartItem => cartItem.id === cartItemToRemove.id ?
-            { ...cartItem, quantity: cartItem.quantity - 1 }
-            : cartItem
-    )
-}
+  const existingCartItem = cartItem.find((cartItem) => cartItem.id === cartItemToRemove.id);
+  if (existingCartItem.quantity === 1) {
+    return cartItem.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+  return cartItem.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
